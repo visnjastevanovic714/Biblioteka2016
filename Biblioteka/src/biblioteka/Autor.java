6f72a34596b4b3838e05fbe56cd43fcb867feb08
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-03-19 16:34:53 +0100

    Uvedene toString i equals metode u klase Knjiga i Autor.

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -1,5 +1,44 @@
 package biblioteka;
 
 public class Autor {
+	String ime;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	String prezime;
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (ime == null) {
+			if (other.ime != null)
+				return false;
+		} else if (!ime.equals(other.ime))
+			return false;
+		if (prezime == null) {
+			if (other.prezime != null)
+				return false;
+		} else if (!prezime.equals(other.prezime))
+			return false;
+		return true;
+	}
 	
 }
--- Biblioteka/src/biblioteka/Knjigu.java
+++ Biblioteka/src/biblioteka/Knjigu.java
@@ -33,5 +33,46 @@ public class Knjigu {
 	public void setIzdanje(String izdanje) {
 		this.izdanje = izdanje;
 	}
+	@Override
+	public String toString() {
+		return "Knjigu [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjigu other = (Knjigu) obj;
+		if (autori == null) {
+			if (other.autori != null)
+				return false;
+		} else if (!autori.equals(other.autori))
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		if (izdanje == null) {
+			if (other.izdanje != null)
+				return false;
+		} else if (!izdanje.equals(other.izdanje))
+			return false;
+		if (izdavac == null) {
+			if (other.izdavac != null)
+				return false;
+		} else if (!izdavac.equals(other.izdavac))
+			return false;
+		if (naslov == null) {
+			if (other.naslov != null)
+				return false;
+		} else if (!naslov.equals(other.naslov))
+			return false;
+		return true;
+	}
 	
 }
